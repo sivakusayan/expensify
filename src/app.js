2fae2dae3b794a01df35438710dd0d938199908c
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-09-21 06:37:41 -0400

    Set up 404 page and links

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
 import './styles/styles.scss';
 
@@ -28,13 +28,33 @@ const HelpPage = () => (
   </div>
 );
 
+const NotFoundPage = () => (
+  <div>
+    404 - <Link to="/">Go to homepage</Link> 
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Add An Expense</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit An Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Get help</NavLink>
+  </header>
+);
+
 const routes = (
   <BrowserRouter>
     <div>
-      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-      <Route path="/create" component={AddExpensePage}/>
-      <Route path="/edit" component={EditExpensePage}/>
-      <Route path="/help" component={HelpPage}/>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+        <Route path="/create" component={AddExpensePage}/>
+        <Route path="/edit" component={EditExpensePage}/>
+        <Route path="/help" component={HelpPage}/>
+        <Route component={NotFoundPage}/>
+      </Switch>
     </div>
   </BrowserRouter>
 );
